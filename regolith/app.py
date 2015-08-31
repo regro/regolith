f4cbb88de5ed6af42962e3502501ab6d29ed9d84
Anthony Scopatz <scopatz@gmail.com>
2015-08-31 16:32:10 -0400

    have some stuff

--- regolith/app.py
+++ regolith/app.py
@@ -1,5 +1,6 @@
 """Flask app for looking at information in regolith."""
 from flask import Flask, request, render_template
+from werkzeug.exceptions import abort
 
 app = Flask('regolith')
 
@@ -20,3 +21,14 @@ def shutdown_server():
 def shutdown():
     shutdown_server()
     return 'Regolith server shutting down...\n'
+
+
+@app.route('/db/<dbname>/coll/<collname>')
+def collection_page(dbname, collname):
+    rc = app.rc
+    try:
+        coll = rc.client[dbname][collname]
+    except (KeyError, AttributeError):
+        abort(404)
+    return render_template('collection.html', rc=rc, dbname=dbname, 
+                           collname=collname, coll=coll)
