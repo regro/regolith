043a807fe8a145c910454e2e33b5608e0dcecad4
Anthony Scopatz <scopatz@gmail.com>
2016-10-22 16:40:30 -0400

    more test attachments

--- regolith/emailer.py
+++ regolith/emailer.py
@@ -1,24 +1,74 @@
 """Emails people via SMTP"""
 import smtplib
+import tempfile
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
 
-MESSAGE = """From: {from}\r\nTo: {to}\r\nSubject: {subject}\r\n\
+try:
+    from docutils.core import publish_string
+except ImportError:
+    publish_string
 
-{body}
-"""
 
-def make_message(rc, to, subject='', body=''):
-    """Creates an email following the approriate format."""
-    kw = {'from': rc.email['from'], 'to': to, 'subject': subject, 'body': body}
-    message = MESSAGE.format(**kw)
-    return (to, message)
+def attach_txt(filename):
+    with open(filename, 'r') as f:
+        txt = f.read()
+    msg = MIMEText(txt, _subtype='text')
+    return msg
+
+
+def attach_pdf(filename):
+    with open(filename, 'r') as f:
+        pdf = f.read()
+    msg = MIMEApplication(pdf, _subtype="pdf")
+    return msg
+
+
+ATTACHERS = {
+    '.txt': attach_txt,
+    '.rst': attach_txt,
+    '.pdf': attach_pdf,
+    }
+
+
+def make_message(rc, to, subject='', body='', attachments=()):
+    """Creates an email following the approriate format. The body kwarg
+    may be a string of restructured text.  Attachements is a list of filenames
+    to attach.
+    """
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = rc.email['from']
+    msg['To'] = to
+    plain = MIMEText(body, 'plain')
+    msg.attach(plain)
+    if publish_string is not None:
+        html = publish_string(body, writer_name='html')
+        html = MIMEText(html, 'html')
+        msg.attach(html)
+    if attachments:
+        text = msg
+        msg = MIMEMultipart('mixed')
+        msg.attach(text)
+        for attachment in attachments:
+            _, ext = os.path.splitext(attachment)
+            att = ATTACHERS[ext](attachment)
+            att.add_header('content-disposition', 'attachment',
+                           filename=attachment)
+            msg.attach(att)
+    return (to, msg.as_string())
 
 
 def test_email(rc):
     """Sends a test email from regolith."""
     if rc.to is None:
         raise ValueError('--to must be given to send a test email.')
-    message = make_message(rc, rc.to, subject='Regolith test email',
-                           body='This is only a test. Do not be alarmed.')
+    with tempfile.NamedTemporaryFile(suffix='.rst') as f:
+        f.write(b'This is *only* a test attachment.')
+        message = make_message(rc, rc.to, subject='Regolith test email',
+                               body='This is *only* a test. Do not be alarmed.',
+                               attachments=[f.name])
     return [message]
 
 
