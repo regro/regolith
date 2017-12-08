6e20ea17e6430e5f201128f6f9d5fc944496cfaf
Anthony Scopatz <scopatz@gmail.com>
2017-12-08 13:35:00 -0500

    bumped version to 0.1.7

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def main():
         description='A research group content management system',
         long_description=readme,
         license='CC0',
-        version='0.1.6',
+        version='0.1.7',
         author='Anthony Scopatz',
         maintainer='Anthony Scopatz',
         author_email='scopatz@gmail.com',
