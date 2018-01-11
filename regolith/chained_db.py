780016afd053b6cc695d395e1ebe76006b6abded
christopher <cjwright4242@gmail.com>
2018-01-11 12:25:23 -0500

    DOC: update doc

--- regolith/chained_db.py
+++ regolith/chained_db.py
@@ -4,7 +4,8 @@ from collections import ChainMap, MutableMapping
 
 
 class ChainDB(ChainMap):
-    """ A ChainMap who's ``_getitem__`` returns an instance of itself"""
+    """ A ChainMap who's ``_getitem__`` returns either a ChainDB or
+    the result"""
 
     def __getitem__(self, key):
         res = None
