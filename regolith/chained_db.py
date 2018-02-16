8de5e85b119bf51a54c02cf0fe0928ca5b0324f8
christopher <cjwright4242@gmail.com>
2018-02-16 15:10:55 -0500

    FIX: fix the regolith app (json.dumps is not chain friendly)

--- regolith/chained_db.py
+++ regolith/chained_db.py
@@ -41,3 +41,13 @@ class ChainDB(ChainMap):
             for mapping in reversed(self.maps):
                 if key in mapping:
                     mapping[key] = value
+
+
+def convert_to_dict(cm):
+    if isinstance(cm, (ChainMap, ChainDB)):
+        r = {}
+        for k, v in cm.items():
+            r[k] = convert_to_dict(v)
+        return r
+    else:
+        return cm
