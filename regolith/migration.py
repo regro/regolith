70901b4660123f971ca5d3a6bc2d50b15bff915e
christopher <cjwright4242@gmail.com>
2018-03-05 15:32:50 -0500

    STY: pep8

--- regolith/migration.py
+++ regolith/migration.py
@@ -1,6 +1,6 @@
 """Maps between old keys and new"""
 
 migration_map = {'proposals': {'day': 'due_day',
-                   'month': 'due_month',
-                   'year': 'due_year'}
+                               'month': 'due_month',
+                               'year': 'due_year'}
                  }
