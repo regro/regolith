b69a31dc7dc2daea5ad792cc19bbdecd6b44919a
christopher <cjwright4242@gmail.com>
2018-05-04 17:32:52 -0400

    TST: run the tests

--- tests/test_builders.py
+++ tests/test_builders.py
@@ -22,21 +22,28 @@ def test_builder(bm, make_db):
     expected_base = os.path.join(os.path.dirname(__file__),
                                  'outputs')
     for root, dirs, files in os.walk('.'):
-        root = root.strip('./')
         for file in files:
+
             # Use this for bootstrapping the tests,
             # confirm by hand that files look correct
-            if root != '.':
-                os.makedirs(os.path.join(expected_base, bm, root), exist_ok=True)
-            if root == '.':
-                shutil.copyfile(os.path.join(file),
-                                os.path.join(expected_base, bm, file))
-            else:
-                shutil.copyfile(os.path.join(root, file),
-                                os.path.join(expected_base, bm, root, file))
-            # if file in os.listdir(os.path.join(expected_base, bm)):
-            #     with open(os.path.join(repo, '_build', bm, file), 'r') as f:
-            #         actual = f.read()
-            #     with open(os.path.join(expected_base, bm, file), 'r') as f:
-            #         expected = f.read()
-            #     assert expected == actual
+            # if root != '.':
+            #     os.makedirs(os.path.join(expected_base, bm, root),
+            #                 exist_ok=True)
+            # if root == '.':
+            #     shutil.copyfile(os.path.join(file),
+            #                     os.path.join(expected_base, bm, file))
+            # else:
+            #     shutil.copyfile(os.path.join(root, file),
+            #                     os.path.join(expected_base, bm, root, file))
+
+            if file in os.listdir(os.path.join(expected_base, bm, root)):
+                with open(os.path.join(repo, '_build', bm, root, file),
+                          'r') as f:
+                    actual = f.read()
+                with open(os.path.join(expected_base, bm, root, file),
+                          'r') as f:
+                    expected = f.read()
+
+                # Skip because of a date time in
+                if file != 'rss.xml':
+                    assert expected == actual
