6b93811d3cb7c51567176cbe0b52c84304d54e0d
christopher <cjwright4242@gmail.com>
2018-05-08 15:21:35 -0400

    Only warn if ``eallowed`` is not obeyed, also test the exemplars.

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 import pytest
 
 from regolith.schemas import SCHEMAS, validate, EXEMPLARS
@@ -6,3 +8,10 @@ from regolith.schemas import SCHEMAS, validate, EXEMPLARS
 @pytest.mark.parametrize('key', SCHEMAS.keys())
 def test_validation(key):
     validate(key, EXEMPLARS[key], SCHEMAS)
+
+
+@pytest.mark.parametrize('key', SCHEMAS.keys())
+def test_exemplars(key):
+    v = validate(key, EXEMPLARS[key], SCHEMAS)
+    pprint(v[1])
+    assert v[0]
