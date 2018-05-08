11fa4ca62e67a41757739aaad37ee1b09b263f73
christopher <cjwright4242@gmail.com>
2018-05-08 16:02:37 -0400

    FIX: need to deepcopy exemplars for submission due to id pop

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -13,7 +13,4 @@ def test_validation(key):
 @pytest.mark.parametrize('key', SCHEMAS.keys())
 def test_exemplars(key):
     v = validate(key, EXEMPLARS[key], SCHEMAS)
-    if not v[0]:
-        pprint(v[1])
-        pprint(EXEMPLARS[key])
     assert v[0]
