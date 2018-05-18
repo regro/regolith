a728400af14db2b642251a3a9ea38b5424b16b35
Simon Billinge <simon.billinge@gmail.com>
2018-05-18 07:41:21 -0400

    BUG: found missing close-brace that was failing tests.
    Also, some pep8 cleaning

--- regolith/schemas.py
+++ regolith/schemas.py
@@ -887,62 +887,64 @@ SCHEMAS = {
                 'required': True,
                 'type': 'string'},
         'abstract': {'description': 'abstract of the presentation',
-                        'required': False,
-                        'type': 'string'},
-        'authors': {'description': 'Author list.  Each entry must be discoverable'
-                                   'in the name or aka of a person',
-                        'required': True,
-                        'type': 'string'},
+                     'required': False,
+                     'type': 'string'},
+        'authors': {
+            'description': 'Author list.  Each entry must be discoverable'
+                           'in the name or aka of a person',
+            'required': True,
+            'type': 'string'},
         'begin_year': {'description': 'year the conference or trip begins.',
-                        'required': True,
-                        'type': 'integer'},
+                       'required': True,
+                       'type': 'integer'},
         'begin_month': {'required': True,
-                       'type': 'string'},
+                        'type': 'string'},
         'begin_day': {'required': False,
-                        'type': 'integer'},
+                      'type': 'integer'},
         'department': {'description': 'department of the institution where the'
                                       'presentation will be made, if '
                                       'applicable.  should be discoverable in '
                                       'institutions.',
-                        'required': False,
-                        'type': 'string'},
-        'end_year': {'description': 'year the conference or trip ends',
-                       'required': False,
-                       'type': 'integer'},
-        'end_month': {'required': False,
-                        'type': 'string'},
-        'end_day': {'required': False,
-                      'type': 'integer'},
-        'institution': {'description': 'institution where the'
-                                       'presentation will be made, if '
-                                       'applicable.  should be discoverable in '
-                                       'institutions.',
                        'required': False,
                        'type': 'string'},
-        'meeting_name': {'description': 'full name of the conference or '
-                                        'meeting.  If it is a departmental '
-                                        'seminar or colloquium, write Seminar'
-                                        'or Colloquium and fill in department '
-                                        'and institution fields',
-                        'required': True,
-                        'type': 'string'},
-        'project': {'description': 'project or list of projects that this '
-                                   'presentation is associated with.  Should be'
-                                   'discoverable in projects collection',
+        'end_year': {'description': 'year the conference or trip ends',
+                     'required': False,
+                     'type': 'integer'},
+        'end_month': {'required': False,
+                      'type': 'string'},
+        'end_day': {'required': False,
+                    'type': 'integer'},
+    },
+    'institution': {'description': 'institution where the'
+                                   'presentation will be made, if '
+                                   'applicable.  should be discoverable in '
+                                   'institutions.',
                     'required': False,
                     'type': 'string'},
-        'title': {'description': 'title of the presentation',
-                  'required': True,
-                  'type': 'string'},
-        'type': {'description': 'type of presentation',
-                 'items': {'type': 'string',
-                           'eallowed': ['award','colloquium', 'contributed_oral',
-                                    'invited', 'keynote','nobel',
+    'meeting_name': {'description': 'full name of the conference or '
+                                    'meeting.  If it is a departmental '
+                                    'seminar or colloquium, write Seminar'
+                                    'or Colloquium and fill in department '
+                                    'and institution fields',
+                     'required': True,
+                     'type': 'string'},
+    'project': {'description': 'project or list of projects that this '
+                               'presentation is associated with.  Should be'
+                               'discoverable in projects collection',
+                'required': False,
+                'type': 'string'},
+    'title': {'description': 'title of the presentation',
+              'required': True,
+              'type': 'string'},
+    'type': {'description': 'type of presentation',
+             'items': {'type': 'string',
+                       'eallowed': ['award', 'colloquium', 'contributed_oral',
+                                    'invited', 'keynote', 'nobel',
                                     'plenary', 'poster',
                                     'seminar']
-                           },
-                 'required': True,
-                 'type': 'string'},
+                       },
+             'required': True,
+             'type': 'string'},
     'projects': {
         '_description': {
             'description': 'This collection describes the research group '
@@ -1071,7 +1073,8 @@ SCHEMAS = {
         'university_id': {
             'description': 'The university identifier for the student',
             'required': False,
-            'type': 'string'}},
+            'type': 'string'}
+    },
 }
 
 
