eeb1792984d0284cb5a6e6cf84a1ba98b2247923
christopher <cjwright4242@gmail.com>
2018-05-25 16:58:31 -0400

    FIX: fix retrieval code

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,6 +1,18 @@
 from regolith.tools import filter_publications
+from regolith.tools import fuzzy_retrieval
 
 
 def test_author_publications():
     citations = [{'author': ['CJ', 'SJLB']}, {'editor': 'SJLB'}]
     filter_publications(citations, {'SJLB'})
+
+
+def test_fuzzy_retrieval():
+    person = {'_id': 'scopatz',
+              'aka': ['Scopatz',
+                      'Scopatz, A',
+                      'Scopatz, A.',
+                      'Scopatz, A M',
+                      'Anthony Michael Scopatz'],
+              'name': 'Anthony Scopatz'}
+    fuzzy_retrieval([person], ['aka', 'name', '_id'], 'scopatz')
