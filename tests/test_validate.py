9e02fb783738f89f62c1c807451a81b097c33961
christopher <cjwright4242@gmail.com>
2018-05-27 17:27:03 -0400

    TST: run validation with python as well

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,9 +1,37 @@
 import os
+from io import StringIO
 import subprocess
+import sys
 
 import pytest
 
 from regolith.main import main
+
+
+def test_validate_python(make_db):
+    repo = make_db
+    os.chdir(repo)
+    backup = sys.stdout
+    sys.stdout = StringIO()
+    main(['validate'])
+    out = sys.stdout.getvalue()
+    sys.stdout.close()
+    sys.stdout = backup
+    assert 'NO ERRORS IN DBS' in out
+
+
+def test_validate_bad_python(make_bad_db):
+    repo = make_bad_db
+    os.chdir(repo)
+    backup = sys.stdout
+    sys.stdout = StringIO()
+    main(['validate'])
+    out = sys.stdout.getvalue()
+    sys.stdout.close()
+    sys.stdout = backup
+    assert 'Errors found in ' in out
+    assert 'NO ERRORS IN DBS' not in out
+
 
 @pytest.mark.skipif(os.name == 'nt',
                     reason="Windows not working with subprocess run")
@@ -22,10 +50,3 @@ def test_validate_bad(make_bad_db):
     out = subprocess.check_output(['regolith', 'validate']).decode('utf-8')
     assert 'Errors found in ' in out
     assert 'NO ERRORS IN DBS' not in out
-
-def test_validate(make_db):
-    repo = make_db
-    os.chdir(repo)
-#    out = main(['validate']).decode('utf-8')
-    out = main(['validate'])
-    assert 'NO ERRORS IN DBS' in out
