248ed512a2d826e7eeb244283cd9d04964e692fc
christopher <cjwright4242@gmail.com>
2018-05-27 17:27:03 -0400

    TST: run validation with python as well

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -55,10 +55,3 @@ def test_validate_bad(make_bad_db):
     out = subprocess.check_output(['regolith', 'validate']).decode('utf-8')
     assert 'Errors found in ' in out
     assert 'NO ERRORS IN DBS' not in out
-
-def test_validate(make_db):
-    repo = make_db
-    os.chdir(repo)
-#    out = main(['validate']).decode('utf-8')
-    out = main(['validate'])
-    assert 'NO ERRORS IN DBS' in out
