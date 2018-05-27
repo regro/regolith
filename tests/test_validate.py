d2738dbbff1040573661f756df54ea61476ca815
Simon Billinge <simon.billinge@gmail.com>
2018-05-27 17:07:58 -0400

    trying to make the tests work on Windows!

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -19,7 +19,12 @@ def test_validate_python(make_db):
     sys.stdout = backup
     assert 'NO ERRORS IN DBS' in out
 
+import pytest
 
+from regolith.main import main
+
+@pytest.mark.skipif(os.name == 'nt',
+                    reason="Windows not working with subprocess run")
 def test_validate_bad_python(make_bad_db):
     repo = make_bad_db
     os.chdir(repo)
@@ -50,3 +55,10 @@ def test_validate_bad(make_bad_db):
     out = subprocess.check_output(['regolith', 'validate']).decode('utf-8')
     assert 'Errors found in ' in out
     assert 'NO ERRORS IN DBS' not in out
+
+def test_validate(make_db):
+    repo = make_db
+    os.chdir(repo)
+#    out = main(['validate']).decode('utf-8')
+    out = main(['validate'])
+    assert 'NO ERRORS IN DBS' in out
