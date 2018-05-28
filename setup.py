d19d1730477231e6285a1dde8182308f94d9e558
christopher <cjwright4242@gmail.com>
2018-05-28 14:30:31 -0400

    bumped version to 0.2.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def main():
         description='A research group content management system',
         long_description=readme,
         license='CC0',
-        version='0.1.11',
+        version='0.2.0',
         author='Anthony Scopatz',
         maintainer='Anthony Scopatz',
         author_email='scopatz@gmail.com',
