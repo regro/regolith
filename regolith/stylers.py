b1d2ce242ab637e96c8ea2344a1c3e2407518346
Simon Billinge <simon.billinge@gmail.com>
2018-06-24 15:56:28 -0400

    sentence case function in stylers and adopted in preslist builder

--- regolith/stylers.py
+++ regolith/stylers.py
@@ -1,10 +1,26 @@
 """A collection of python stylers"""
 
+import re
 
 
+def sentencecase(string):
+    """returns a string in sentencecase but with text in braces preserved
 
-detitle = pres['title'].split()
-detitlelow = [word.lower() if word[0] != "{" else word for word in detitle]
-retitle = ' '.join(detitlelow)
-pres['title'] = retitle[0].capitalize() + retitle[1:]
-
+    parameters
+    ----------
+    string
+        The string
+    :returns
+        The string in sentence-case (but preserving any text wrapped in braces
+    """
+    freezecaps = re.findall("\{[^{}]+\}", string)
+    datalow = string.capitalize()
+    sentencecase = re.split("\{[^{}]+\}", datalow)
+    title = ''
+    freezecaps.reverse()
+    for word in sentencecase:
+        if len(freezecaps) > 0:
+            title = title + word + freezecaps.pop()
+        else:
+            title = title + word
+    return title
