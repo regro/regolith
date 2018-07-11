41a3ffce965f87f8ce34b59ef2f1c206fc3f3f89
christopher <cjwright4242@gmail.com>
2018-07-11 12:36:33 -0400

    more black

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -7,9 +7,10 @@ from regolith.runcontrol import load_rcfile, DEFAULT_RC
 def test_cmd(make_db, tmpdir):
     repo = make_db
     os.chdir(repo)
-    subprocess.check_call(['touch', 'myfile2.tex'], cwd=tmpdir)
-    subprocess.check_call(['regolith', 'store', 'store',
-                           os.path.join(tmpdir, 'myfile2.tex')])
+    subprocess.check_call(["touch", "myfile2.tex"], cwd=tmpdir)
+    subprocess.check_call(
+        ["regolith", "store", "store", os.path.join(tmpdir, "myfile2.tex")]
+    )
     rc = DEFAULT_RC
-    rc._update(load_rcfile('regolithrc.json'))
-    assert 'myfile.tex' in os.listdir(storage_path(find_store(rc), rc))
+    rc._update(load_rcfile("regolithrc.json"))
+    assert "myfile.tex" in os.listdir(storage_path(find_store(rc), rc))
