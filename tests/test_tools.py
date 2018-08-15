a77736aee16bca812e36cfd04f2daf1b724a4c58
christopher <cjwright4242@gmail.com>
2018-08-15 15:48:06 -0400

    properly wrap URLs and allow option to not wrap

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -58,15 +58,23 @@ def test_number_suffix(input, expected):
 
 
 @pytest.mark.parametrize(
-    "input,expected",
+    "input,expected,kwargs",
     [
-        ('$hi', r'\$hi'),
+        ('$hi', r'\$hi', {}),
         (r'Website: https://github.com/CJ-Wright/'
          r'Masters_Thesis/raw/master/thesis.pdf hi',
          r'Website: \url{https://github.com/CJ-Wright/'
-         r'Masters_Thesis/raw/master/thesis.pdf} hi')
+         r'Masters_Thesis/raw/master/thesis.pdf} hi', {}),
+        (r'Website: https://github.com/CJ-Wright/'
+         r'Masters_Thesis/raw/master/thesis.pdf hi',
+         r'Website: \href{https://github.com/CJ-Wright/'
+         r'Masters_Thesis/raw/master/thesis.pdf} hi', {'wrapper': 'href'}),
+        (r'Website: https://github.com/CJ-Wright/'
+         r'Masters_Thesis/raw/master/thesis.pdf hi',
+         r'Website: https://github.com/CJ-Wright/'
+         r'Masters\_Thesis/raw/master/thesis.pdf hi', {'url_check': False})
     ],
 )
-def test_latex_safe(input, expected):
-    output = latex_safe(input)
+def test_latex_safe(input, expected, kwargs):
+    output = latex_safe(input, **kwargs)
     assert output == expected
