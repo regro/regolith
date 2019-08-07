232565aa2c68aeac32c1728fb004cc4615ca8a87
Simon Billinge <simon.billinge@gmail.com>
2019-08-07 17:52:12 -0400

    fixed misplaced list type in employment schema.  Should pass validate now

--- regolith/schemas.py
+++ regolith/schemas.py
@@ -1610,6 +1610,7 @@ SCHEMAS = {
             "description": "Employment information, similar to educational "
                            "information.",
             "required": False,
+            "type": "list",
             "schema": {
                 "type": "dict",
                 "schema": {
@@ -1644,7 +1645,6 @@ SCHEMAS = {
                         "visitor-supported",
                         "visitor-unsupported"],
                     },
-                    "type": "list",
                 },
             },
         },
