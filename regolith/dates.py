15881de2e06f6461e29752bf657a95f2bff5f431
Simon Billinge <simon.billinge@gmail.com>
2019-12-01 15:10:37 -0500

    adding function to get begin and end dates and creating tests for all functions in dates

--- regolith/dates.py
+++ regolith/dates.py
@@ -3,6 +3,9 @@ import calendar
 import datetime
 from dateutil import parser as date_parser
 
+import datetime as dt
+import calendar
+
 MONTHS = {
     "jan": 1,
     "jan.": 1,
@@ -80,7 +83,38 @@ def date_to_float(y, m, d=0):
     y = int(y)
     m = month_to_int(m)
     d = int(d)
-    return y + (m / 100.0) + (d / 100000.0)
+    return y + (m / 100.0) + (d / 10000.0)
+
+
+def begin_end_date(doc):
+    '''returns the beginning amd ending date from a document as date objects
+
+    Assumes, for example, regolith standard, that begin date info is in keys
+    "begin_year", "begin_month" and "begin_day".
+
+    Currently, keys have to be at the top level of the document
+
+    Parameters
+    ----------
+    doc
+      The document from which we want to extract the beginning and ending date
+
+    Returns
+    -------
+       The begin and end-date as datetime.date object
+    '''
+    by = str(doc.get('begin_year'))
+    bm = str(month_to_int(doc.get('begin_month')))
+    bd = str(doc.get('begin_day', 1))
+    ey = doc.get('end_year')
+    em = month_to_int(doc.get('end_month'))
+    month_last_day = calendar.monthrange(ey, em)[1]
+    ed = str(doc.get('end_day', month_last_day))
+    startdate = dt.datetime.strptime(by + bm + bd,
+                                     '%Y%m%d').date()
+    enddate = dt.datetime.strptime(str(ey) + str(em) + ed,
+                                   '%Y%m%d').date()
+    return startdate, enddate
 
 
 def find_gaps_overlaps(dateslist, overlaps_ok=False):
