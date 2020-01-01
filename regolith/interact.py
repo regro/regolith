ec5ccd160ae31bc884b2ff63a256b242d12508c5
Simon Billinge <simon.billinge@gmail.com>
2020-01-01 14:26:15 -0500

    moving stuff around so that the limited load can be made to work in scripts that utilize chained_db from interactive mode

--- regolith/interact.py
+++ regolith/interact.py
@@ -1,13 +1,14 @@
 """
 Loads the dbs for interactive sessions
 """
-from regolith.database import connect
-from regolith.runcontrol import DEFAULT_RC, load_rcfile, filter_databases
+from regolith.runcontrol import DEFAULT_RC, load_rcfile, filter_databases, \
+    connect_db
 
 rc = DEFAULT_RC
 rc._update(load_rcfile("regolithrc.json"))
 filter_databases(rc)
 
-with connect(rc) as rc.client:
-    dbs = rc.client.dbs
-    chained_db = rc.client.chained_db
+chained_db, dbs = connect_db(rc)
+
+
+
