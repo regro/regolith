2a9dc9eafcfef8c3c06067622c8e459c76faf87c
Simon Billinge <simon.billinge@gmail.com>
2020-01-06 18:17:30 -0500

    test for connect_db

--- tests/test_runcontrol.py
+++ tests/test_runcontrol.py
@@ -4,18 +4,22 @@ import sys
 from io import StringIO
 
 from regolith.main import main
+from regolith.broker import load_db
 from regolith.runcontrol import DEFAULT_RC, load_rcfile, filter_databases, \
     connect_db
+from regolith.database import connect
 
 
 def test_connect_db(make_db):
+    repo = make_db
+    os.chdir(repo)
     rc = DEFAULT_RC
+#    print(rc)
     rc._update(load_rcfile("regolithrc.json"))
     filter_databases(rc)
-    expected_chdb = rc.client.chained_db
-    expected_dbs = rc.client.dbs
-
+    with connect(rc) as rc.client:
+        expected_dbs = rc.client.dbs
+        expected_chdb = rc.client.chained_db
     chained_db, dbs = connect_db(rc)
     assert chained_db == expected_chdb
     assert dbs == expected_dbs
-
