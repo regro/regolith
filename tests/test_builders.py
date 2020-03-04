22b589d67666c74f902225c61d82b20e66fc8bc6
Simon Billinge <simon.billinge@gmail.com>
2020-03-04 12:08:36 -0500

    changing tests so that recent collabs will run with scopatz as person

--- tests/test_builders.py
+++ tests/test_builders.py
@@ -52,7 +52,7 @@ def test_builder(bm, make_db):
         prep_figure()
     if bm == "html":
         os.makedirs("templates/static", exist_ok=True)
-    if bm == "reimb":
+    if bm == "reimb" or bm == "recent-collabs":
         subprocess.run(["regolith", "build", bm, "--no-pdf", "--people",
                         "scopatz"], check=True, cwd=repo )
     else:
@@ -96,7 +96,7 @@ def test_builder_python(bm, make_db):
         prep_figure()
     if bm == "html":
         os.makedirs("templates/static", exist_ok=True)
-    if bm == "reimb":
+    if bm == "reimb" or bm == "recent-collabs":
         main(["build", bm, "--no-pdf", "--people", "scopatz"])
     else:
         main(["build", bm, "--no-pdf"])
