2526a9a699638dab98abe896f4421423924cc77e
Simon Billinge <simon.billinge@gmail.com>
2020-04-18 06:30:06 -0400

    clean journal club in template

---
+++ regolith/helper.py
@@ -0,0 +1,38 @@
+"""Generic builder."""
+
+from regolith.builders.cvbuilder import CVBuilder
+from regolith.builders.htmlbuilder import HtmlBuilder
+from regolith.builders.postdocadbuilder import PostdocadBuilder
+from regolith.builders.preslistbuilder import PresListBuilder
+from regolith.builders.publistbuilder import PubListBuilder
+from regolith.builders.gradebuilder import GradeReportBuilder
+from regolith.builders.manuscriptreviewbuilder import ManRevBuilder
+from regolith.builders.proposalreviewbuilder import PropRevBuilder
+from regolith.builders.reimbursementbuilder import ReimbursementBuilder
+from regolith.builders.resumebuilder import ResumeBuilder
+from regolith.builders.cpbuilder import CPBuilder
+from regolith.builders.figurebuilder import FigureBuilder
+from regolith.builders.internalhtmlbuilder import InternalHtmlBuilder
+
+
+BUILDERS = {
+    "cv": CVBuilder,
+    "html": HtmlBuilder,
+    "internalhtml": InternalHtmlBuilder,
+    "publist": PubListBuilder,
+    "grade": GradeReportBuilder,
+    "grades": GradeReportBuilder,
+    "resume": ResumeBuilder,
+    "current-pending": CPBuilder,
+    "postdocad": PostdocadBuilder,
+    "review-man": ManRevBuilder,
+    "review-prop": PropRevBuilder,
+    "preslist": PresListBuilder,
+    "reimb": ReimbursementBuilder,
+    "figure": FigureBuilder,
+}
+
+
+def builder(btype, rc):
+    """Returns builder of the appropriate type."""
+    return BUILDERS[btype](rc)
