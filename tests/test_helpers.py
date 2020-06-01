88ddf584dd29d042f52e7873b13f240f1a36220a
dy2403 <dy2403@barnard.edu>
2020-06-01 14:51:35 -0300

    solving conflicts

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -35,7 +35,11 @@ helper_map = [
     (["helper", "l_grants", "-c", "-d", "2020-05-25"],
      "sym2.0, awardnr: , acctn: , 2019-06-01 to 2030-12-31\n, awardnr: , acctn: GG012345, 2015-10-01 to 2025-09-30\n"
      ),
-
+    (["helper", "a_person", "aeinstein", "Albert Einstein",
+      "Theoretical physicist who developed the theory of relativity",
+      'PhD', 'uzurich', '1905',"A. Einstein", "Einstein"],
+       "aeinstein has been added in person\n"
+     ),
 ]
 
 
