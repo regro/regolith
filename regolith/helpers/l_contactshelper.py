cb35f1f807bb2ab4d0c6db039ce96a18da8a0272
Shomik Ghose <shogho0@gmail.com>
2020-06-06 15:26:23 -0500

    Put fragment_retrieval into the helper

--- regolith/helpers/l_contactshelper.py
+++ regolith/helpers/l_contactshelper.py
@@ -1,31 +1,48 @@
 """Helper for finding and listing contacts from the contacts.yml database.
 Prints name, institution, and email (if applicable) of the contact.
 """
-import datetime 
+import datetime
 import dateutil
 import dateutil.parser as date_parser
-from dateutil.relativedelta import relativedelta
-import sys
-
+from collections import Counter
 
 from regolith.helpers.basehelper import SoutHelperBase
 from regolith.fsclient import _id_key
 from regolith.tools import (
     all_docs_from_collection,
     get_pi_id,
-    fuzzy_retrieval,
+    fragment_retrieval
 )
 
 TARGET_COLL = "contacts"
 HELPER_TARGET = "l_contacts"
 
+
 def subparser(subpi):
-    subpi.add_argument("-n", "--name", help='list of names or name fragments (multiple arguments allowed) to use to find contacts', nargs = '+')
-    subpi.add_argument("-i", "--inst", help='institution or an institution fragment (single argument only) to use to find contacts')
-    subpi.add_argument("-d", "--date", help='approximate date in ISO format (YYYY-MM-DD) corresponding to when the contact was entered in the database. Comes with a default range of 4 months centered around the date; change range using --range argument')
-    subpi.add_argument("-r", "--range", help='range (in months) centered around date d specified by --date, i.e. (d +/- r/2)')
-    subpi.add_argument("-o", "--notes", help='use list of notes or miscellaneous information to use (multiple arguments allowed) to find contacts', nargs = '+')
+    subpi.add_argument(
+        "-n",
+        "--name",
+        help='list of names or name fragments (multiple arguments allowed) to use to find contacts',
+        nargs='+')
+    subpi.add_argument(
+        "-i",
+        "--inst",
+        help='institution or an institution fragment (single argument only) to use to find contacts')
+    subpi.add_argument(
+        "-d",
+        "--date",
+        help='approximate date in ISO format (YYYY-MM-DD) corresponding to when the contact was entered in the database. Comes with a default range of 4 months centered around the date; change range using --range argument')
+    subpi.add_argument(
+        "-r",
+        "--range",
+        help='range (in months) centered around date d specified by --date, i.e. (d +/- r/2)')
+    subpi.add_argument(
+        "-o",
+        "--notes",
+        help='list of fragments (multiple arguments allowed) to be found in the notes section of a contact',
+        nargs='+')
     return subpi
+
 
 def is_current(thing, now=None):
     """
@@ -47,11 +64,13 @@ def is_current(thing, now=None):
     dates = thing
     current = False
     try:
-        if dates.get("begin_date") <= now <= dates.get("end_date", datetime.date(5000, 12, 31)):
+        if dates.get("begin_date") <= now <= dates.get(
+                "end_date", datetime.date(5000, 12, 31)):
             current = True
-    except:
+    except BaseException:
         raise RuntimeError(f"Cannot find begin_date in document:\n {thing}")
     return current
+
 
 class ContactsListerHelper(SoutHelperBase):
     """Helper for finding and listing contacts from the contacts.yml file
@@ -71,7 +90,7 @@ class ContactsListerHelper(SoutHelperBase):
         try:
             if not rc.database:
                 rc.database = rc.databases[0]["name"]
-        except:
+        except BaseException:
             pass
         colls = [
             sorted(
@@ -86,46 +105,63 @@ class ContactsListerHelper(SoutHelperBase):
         gtx["str"] = str
         gtx["zip"] = zip
 
-
     def sout(self):
         rc = self.rc
         contacts = []
+        ret_list = []
+        num = 0
+        if rc.name:
+            num += 1
+            for name in rc.name:
+                contacts.extend(
+                    fragment_retrieval(
+                        self.gtx['contacts'], [
+                            "_id", "aka", "name"], name))
+        if rc.inst:
+            num += 1
+            contacts.extend(
+                fragment_retrieval(
+                    self.gtx['contacts'],
+                    ["institution"],
+                    rc.inst))
+        if rc.notes:
+            num += 1
+            for note in rc.notes:
+                contacts.extend(
+                    fragment_retrieval(
+                        self.gtx['contacts'],
+                        ["notes"],
+                        note))
         if rc.date:
+            num += 1
             temp_dat = date_parser.parse(rc.date).date()
-        for contact in self.gtx["contacts"]:
-            if rc.name:
-                list_add = False
-                for nam in rc.name:
-                     if nam.casefold() in contact.get('name').casefold():
-                        list_add = True
-                        break
-                if not list_add:
-                    continue
-            if rc.inst:
-                if not (rc.inst.casefold() in contact.get('institution').casefold()):
-                    continue
-            if rc.date:
-                if rc.range:
-                    temp_dict = {"begin_date":temp_dat - dateutil.relativedelta.relativedelta(months=int(rc.range)), "end_date":temp_dat + dateutil.relativedelta.relativedelta(months=int(rc.range))} 
-                else:
-                    temp_dict = {"begin_date":temp_dat - dateutil.relativedelta.relativedelta(months=2), "end_date":temp_dat + dateutil.relativedelta.relativedelta(months=2)}                
-                if not is_current(temp_dict, now=temp_dat):
-                    continue
-            if rc.notes:
-                list_add = False
-                for note in rc.notes:
-                    if isinstance(contact.get('notes'),str):
-                        if note.casefold() in contact.get('notes').casefold():
-                            list_add = True
-                    elif isinstance(contact.get('notes'),list):
-                        for no in contact.get('notes'):
-                             if note.casefold() in no.casefold():
-                                 list_add = True
-                if not list_add:
-                    continue
-            contacts.append(contact)
+            for contact in self.gtx["contacts"]:
+                if rc.date:
+                    if rc.range:
+                        temp_dict = {
+                            "begin_date": temp_dat -
+                            dateutil.relativedelta.relativedelta(
+                                months=int(
+                                    rc.range)),
+                            "end_date": temp_dat +
+                            dateutil.relativedelta.relativedelta(
+                                months=int(
+                                    rc.range))}
+                    else:
+                        temp_dict = {
+                            "begin_date": temp_dat -
+                            dateutil.relativedelta.relativedelta(
+                                months=2),
+                            "end_date": temp_dat +
+                            dateutil.relativedelta.relativedelta(
+                                months=2)}
+                    if is_current(temp_dict, now=temp_dat):
+                        contacts.append(contact)
         for con in contacts:
-            if con.get('email'):
-                print(f"name: {con.get('name')}, institution: {con.get('institution')}, email: {con.get('email','missing')}")
+            ret_list.append(
+                f"name: {con.get('name')}, institution: {con.get('institution')}, email: {con.get('email','missing')}")
+        temp_c = Counter(ret_list)
+        for key in temp_c:
+            if temp_c[key] == num:
+                print(temp_c[key])
         return
-
