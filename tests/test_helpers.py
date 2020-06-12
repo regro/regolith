dfad80d07e6dce0b4ed9cf3eb30a36a99b30c7f9
st3107 <st3107@columbia.edu>
2020-06-11 20:49:34 -0400

    BUG: fix syntax

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -46,7 +46,6 @@ helper_map = [
      "Anthony Scopatz, professor | group_id: scopatz\n"
      "    orcid: 0000-0002-9432-4248 | github_id: ascopatz\n"
      ),
-    (["helper", "l_contacts", "run", "-n", "ny", "-i", "col", "-o", "coffee", "-d", "2020-01-15", "-r", "2"]),
     (["helper", "l_members", "-f"],
      "Anthony Scopatz\n"
      "    current organization: The University of South Carolina\n"
