371482a334b22510bb7f030aada0035d6334ea57
Simon Billinge <simon.billinge@gmail.com>
2020-06-14 11:22:33 -0400

    adding windows skips to test_validate and test_storage

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,9 +1,11 @@
 import os
 import subprocess
+import pytest
+import sys
 from regolith.storage import find_store, storage_path
 from regolith.runcontrol import load_rcfile, DEFAULT_RC
 
-
+@pytest.mark.skipif(sys.platform == "win32", reason="does not run on windows")
 def test_cmd(make_db, tmpdir):
     repo = make_db
     os.chdir(repo)
