8dd107b9dfc2a7a0525147160e65edcb329ffca0
berrakozer <bo2220@columbia.edu>
2020-06-16 17:27:12 -0400

    added billinge group position to former members and made sure current position printed out for both current and former

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -81,10 +81,6 @@ helper_map = [
     "    current position: Assistant Professor, Mechanical Engineering Department\n"
     "    billinge group position: Fake Billinge Group position\n"
      ),
-    (["helper", "l_members", "-f"],
-     "Anthony Scopatz\n"
-     "    current organization: The University of South Carolina\n"
-     "    current position: Assistant Professor, Mechanical Engineering Department\n"),
     (["helper", "l_contacts", "run", "-n", "ny", "-i", "col", "-o", "coffee", "-d", "2020-01-15", "-r", "2"],
      "name: Anthony B Friend, institution: columbiau, email: friend@deed.com\n"
      ),
