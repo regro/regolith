f7c5f597cd919f8a7d4849abcc175dc78e77fc49
zichengliu1997 <51381871+zichengliu1997@users.noreply.github.com>
2020-06-18 12:19:59 -0400

    new stuff

--- regolith/helper.py
+++ regolith/helper.py
@@ -3,6 +3,7 @@
 from regolith.helpers import hellohelper as hello
 from regolith.helpers import a_proprevhelper as a_proprev
 from regolith.helpers import a_grppub_readlisthelper as a_gprl
+from regolith.helpers import a_manurevhelper as a_manuscript
 from regolith.helpers import a_projectumhelper as a_projectum
 from regolith.helpers import a_proposalhelper as a_proposal
 from regolith.helpers import l_milestoneshelper as l_milestone
@@ -17,6 +18,7 @@ HELPERS = {
     "hello": (hello.HelloHelper, hello.subparser),
     "a_proprev": (a_proprev.PropRevAdderHelper, a_proprev.subparser),
     "a_grppub_readlist": (a_gprl.GrpPubReadListAdderHelper, a_gprl.subparser),
+    "a_manuscript": (a_manuscript.ManuRevAdderHelper, a_manuscript.subparser),
     "a_projectum": (a_projectum.ProjectumAdderHelper, a_projectum.subparser),
     "a_proposal": (a_proposal.ProposalAdderHelper, a_proposal.subparser),
     "l_milestones": (l_milestone.MilestonesListerHelper, l_milestone.subparser),
