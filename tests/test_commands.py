e401dc0454f404365fcf26012d45a219a5d1c8b9
st3107 <st3107@columbia.edu>
2020-06-25 12:23:38 -0400

    TST: add start server in test

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -9,5 +9,9 @@ def test_fs_to_mongo(make_db):
         repo = str(Path(__file__).parent.parent.parent.joinpath('rg-db-group', 'local'))
     else:
         repo = make_db
-    cp = subprocess.run(['regolith', 'fs-to-mongo'], cwd=repo)
-    assert cp.returncode == 0
+    cp0 = subprocess.run(['mongod'])
+    if cp0.returncode != 0:
+        print("Server failed to start.")
+        return
+    cp1 = subprocess.run(['regolith', 'fs-to-mongo'], cwd=repo)
+    assert cp1.returncode == 0
