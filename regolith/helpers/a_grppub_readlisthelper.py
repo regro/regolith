7b31576d787faf1f59c9616f5a3c45a144fa7c6d
Jiawei <jz3122@columbia.edu>
2020-07-07 23:26:19 +0800

    add test file for reading-list builder

--- regolith/helpers/a_grppub_readlisthelper.py
+++ regolith/helpers/a_grppub_readlisthelper.py
@@ -76,13 +76,13 @@ class GrpPubReadListAdderHelper(DbHelperBase):
         else:
             pdoc.update({'purpose': ''})
         pdoc.update({"_id": key})
-        pdoc.update({'papers': {}})
+        papers = []
 
         for cite in self.gtx["citations"]:
             for tag in rc.tags:
                 if tag in cite.get("tags"):
-                    pdoc["papers"].update({"doi": cite.get("doi"),
-                                           "text": cite.get("synopsis")})
+                    papers.append({"doi": cite.get("doi"), "text": cite.get("synopsis")})
+                    pdoc.update({'papers': papers})
         rc.client.insert_one(rc.database, rc.coll, pdoc)
 
         print("{} has been added in reading_lists".format(
