0335c9f2f8793ae69284a10ae038e86714dabbfe
Jiawei Zang <jz3122@columbia.edu>
2020-07-28 19:44:30 +0800

    improve sorting in todolist

--- regolith/helpers/f_todohelper.py
+++ regolith/helpers/f_todohelper.py
@@ -87,7 +87,7 @@ class TodoFinisherHelper(DbHelperBase):
                 if type(todo["due_date"]) == str:
                     todo["due_date"] = date_parser.parse(todo["due_date"]).date()
                 todo["days_to_due"] = (todo.get('due_date') - today).days
-                todo["order"] = todo['importance'] + 1 / (1 + math.exp(abs(todo["days_to_due"])))
+                todo["order"] = todo['importance'] + 1 / (1 + math.exp(abs(todo["days_to_due"]-0.5)))-(todo["days_to_due"] < -7)*10
             todolist = sorted(todolist, key=lambda k: (-k['order'], k.get('duration', 10000)))
             print("If the indices are far from being in numerical order, please reorder them by running regolith helper u_todo -r")
             print("Please choose from one of the following to update:")
