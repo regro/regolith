c89332d7cf7d2749c7037922bf1a4ea40cbd9c05
priyankaanehra <priyankaanehra@gmail.com>
2021-01-11 14:20:47 -0500

    First test passed with a single command line argument for title

--- regolith/helpers/l_abstracthelper.py
+++ regolith/helpers/l_abstracthelper.py
@@ -1,6 +1,3 @@
-# add author change id to name?
-# no location leads to no output -fix and make roboust
-#get tests to pass
 
 from regolith.helpers.basehelper import SoutHelperBase
 from regolith.fsclient import _id_key
@@ -40,14 +37,12 @@ def subparser(subpi):
         help='Fragment of the title of the abstract or talk to use to filter presentations.')
     return subpi
 
-
-
 class AbstractListerHelper(SoutHelperBase):
     """Helper for finding and listing abstracts from the presentations.yml file
     """
     # btype must be the same as helper target in helper.py
     btype = HELPER_TARGET
-    needed_dbs = [f'{TARGET_COLL}', 'authors']
+    needed_dbs = [f'{TARGET_COLL}', 'people', 'contacts']
 
     def construct_global_ctx(self):
         """Constructs the global context"""
@@ -59,7 +54,7 @@ class AbstractListerHelper(SoutHelperBase):
         rc.coll = f"{TARGET_COLL}"
         try:
             if not rc.database:
-                rc.database = rc.databases[0]["authors"]
+                rc.database = rc.databases[0]
         except BaseException:
             pass
         colls = [
@@ -77,29 +72,17 @@ class AbstractListerHelper(SoutHelperBase):
 
     def sout(self):
         rc = self.rc
-        collection = self.gtx["presentations"]
+        presentations = self.gtx["presentations"]
 
-        if rc.year is None:
-            rc.year = ''
-        if rc.location is None:
-            rc.location = ''
-        if rc.title is None:
-            rc.title = ''
-
-        for key in collection:
-            if rc.author in key[1]['authors']:
-                if "location" in key[1] and rc.location.lower() in key[1]["location"].lower():
-                    if rc.title.lower() in key[1]["title"].lower():
-                        if "end_year" in key[1] and "begin_year" in key[1]:
-                            if rc.year in str(key[1]["end_year"]) or rc.year in str(key[1]["begin_year"]):
-                                print("---------------------------------------")
-                                if "title" in key[1]:
-                                    print("Title: " + key[1]["title"]+"\n")
-                                a = ""
-                                for i in range(0, len(key[1]["authors"])):
-                                    auth=key[1]["authors"][i]
-                                    auth=get_person_contact(auth, self.gtx["people"], self.gtx["contacts"])
-                                    a = a + auth + ", "
-                                print(a)
-                                print("\nAbstract: " + key[1]["abstract"])
+        for presentation in presentations:
+            if rc.title.casefold() not in presentation.get('title').casefold():
+                continue
+            print("---------------------------------------")
+            print(f"Title: {presentation.get('title')}\n")
+            author_list=[author
+                         if not get_person_contact(author, self.gtx["people"], self.gtx["contacts"])
+                         else get_person_contact(author, self.gtx["people"], self.gtx["contacts"]).get('name')
+                         for author in presentation.get('authors')]
+            print(", ".join(author_list))
+            print(f"\nAbstract: {presentation.get('abstract')}")
         return
