64867fc6a599c29fb601e2af1b59b627342ee671
PeterCheaterPumpkinEater <peter51198@gmail.com>
2021-08-10 14:02:35 -0400

    Added a 'semester' flag to currentapppointmentshelper

--- regolith/helpers/currentappointmentshelper.py
+++ regolith/helpers/currentappointmentshelper.py
@@ -5,12 +5,17 @@ from regolith.sorters import position_key
 from datetime import date, timedelta
 from regolith.helpers.makeappointmentshelper import _future_grant
 from regolith.fsclient import _id_key
+import calendar
 
+SEMESTER_START_MONTH = {'fall': (9, 12), 'spring': (1, 5), 'summer': (6, 8)}
 
 def subparser(subpi):
     subpi.add_argument("-d", "--date",
                        help="the date from which its current appointments will be listed, "
                             "defaults to today's date")
+    subpi.add_argument("-s", "--semester",
+                       help="list all the appointments for the semester of the specified date",
+                       action="store_true")
     return subpi
 
 class CurrentAppointmentsHelper(SoutHelperBase):
@@ -44,10 +49,20 @@ class CurrentAppointmentsHelper(SoutHelperBase):
 
     def sout(self):
         rc = self.rc
+        since, before = None, None
         if rc.date:
             ondate = date(*[int(num) for num in rc.date.split('-')])
         else:
             ondate = date.today()
+
+        if rc.semester:
+            for v in SEMESTER_START_MONTH.values():
+                if v[0] <= ondate.month <= v[1]:
+                    since = date(ondate.year, v[0], 1)
+                    last_day = calendar.monthrange(ondate.year, v[1])[0]
+                    before = date(ondate.year, v[1],
+                                  last_day)
+
         people = self.gtx['people']
         jg = self.gtx['grants']
         proposals = self.gtx["proposals"]
@@ -80,8 +95,13 @@ class CurrentAppointmentsHelper(SoutHelperBase):
                     appt_dates = get_dates(appt)
                     bd = appt_dates.get('begin_date')
                     ed = appt_dates.get('end_date')
-                    if ondate >= bd:
-                        if ondate <= ed:
+                    if since and before:
+                        if (ed >= since) and (bd <= before):
+                            print(person.get('_id'), grantid, accountnr, loading,
+                                  bd.strftime("%Y-%m-%d"),
+                                  ed.strftime("%Y-%m-%d"))
+                    else:
+                        if bd <= ondate <= ed:
                             print(person.get('_id'), grantid, accountnr, loading,
                                   bd.strftime("%Y-%m-%d"),
                                   ed.strftime("%Y-%m-%d"))
