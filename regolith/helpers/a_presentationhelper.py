bf0ec5295e0c5f152295fe01ca859590d550b72a
Simon Billinge <simon.billinge@gmail.com>
2021-08-17 10:16:58 -0400

    on the way to a better workflow

--- regolith/helpers/a_presentationhelper.py
+++ regolith/helpers/a_presentationhelper.py
@@ -89,12 +89,6 @@ def subparser(subpi):
     subpi.add_argument("--no_cal",
                        help=f"Do not add the presentation to google calendar",
                        action="store_true")
-    subpi.add_argument("--activate_google_calendar",
-                       help=f"Run the google calendar api authentication flow"
-                            f"Do this if this is your first time using the google calendar feature"
-                            f"This only has to be done once. After authenticating, the token is stored"
-                            f"in ~/.config/regolith/tokens",
-                       action="store_true")
     return subpi
 
 
@@ -124,9 +118,20 @@ class PresentationAdderHelper(DbHelperBase):
         gtx["zip"] = zip
 
     def db_updater(self):
-        rc = self.rc
         gtx = self.gtx
         rc = self.rc
+        if not rc.no_cal:
+            event = {
+                        'summary': rc.name,
+                        'location': rc.place,
+                        'start': {'date': rc.begin_date},
+                        'end': {'date': rc.end_date}
+                    }
+            cal_update_bool = add_to_google_calendar(event)
+            if not cal_update_bool:
+                google_cal_auth_flow()
+                add_to_google_calendar(event)
+
         # dates
         begin_date = date_parser.parse(rc.begin_date).date()
         end_date = date_parser.parse(rc.end_date).date()
@@ -140,8 +145,13 @@ class PresentationAdderHelper(DbHelperBase):
                     "WARNING: no person been set. please rerun specifying authors,"
                     "or add your id, e.g., sbillinge, to the config.json file in ~/.config/regolith"
                 )
+        split_person = rc.person.strip().split()
+        if len(split_person) >= 2:
+            name_key = split_person[0][0].casefold() + split_person[-1][0].casefold()
+        else:
+            name_key = split_person[0][:1].casefold()
 
-        key = f"{str(begin_date.year)[2:]}{str(begin_date.strftime('%m'))}{rc.person[0:2]}_{''.join(rc.place.casefold().split()).strip()}"
+        key = f"{str(begin_date.year)[2:]}{str(begin_date.strftime('%m'))}{name_key}_{''.join(rc.place.casefold().split()).strip()}"
         coll = self.gtx[rc.coll]
         pdocl = list(filter(lambda doc: doc["_id"] == key, coll))
         if len(pdocl) > 0:
@@ -191,17 +201,5 @@ class PresentationAdderHelper(DbHelperBase):
             edoc = expense_constructor(key, begin_date, end_date, rc)
             rc.client.insert_one(rc.database, EXPENSES_COLL, edoc)
             print(f"{key} has been added in {EXPENSES_COLL}")
-
-        if rc.activate_google_calendar:
-            google_cal_auth_flow()
-
-        if not rc.no_cal:
-            event = {
-                        'summary': rc.name,
-                        'location': rc.place,
-                        'start': {'date': rc.begin_date},
-                        'end': {'date': rc.end_date}
-                    }
-            add_to_google_calendar(event)
 
         return
--- regolith/tools.py
+++ regolith/tools.py
@@ -2044,18 +2044,21 @@ def add_to_google_calendar(event):
         else:
             print('The google calendar feature needs authentication information to run. '
                   'This needs to be done just once for each new device. '
+                  'After granting permission to regolith to access your calendar '
+                  'hit continue to continue'
                   'To do this, please type:\n\n'
                   '   regolith helper a_presentation --activate-google-calendar\n\n'
-                  'and follow the prompts in the browser popup from Google.'
-                  ' You can then rerun this command and it should work, now and in the future.\n'
+                  'and follow the prompts in the browser popup from Google. '
+                  'You can then rerun this command and it should work, now and in the future.\n'
                   'The helper may be run without creating a calendar event by rerunning the command (e.g., use up-arrow) and adding --no-cal')
-            return
+            return 0
         with open(tokenfile, 'w') as token:
             token.write(creds.to_json())
 
     service = build('calendar', 'v3', credentials=creds)
     event = service.events().insert(calendarId='primary', body=event).execute()
     print('Event created: %s' % (event.get('htmlLink')))
+    return 1
 
 def google_cal_auth_flow():
     """First time authentication, this function opens a window to request user consent to use google calendar API,
