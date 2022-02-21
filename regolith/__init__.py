b2dc31cc42861704a078cc0b18468afc0a4d413a
Simon Billinge <sb2896@columbia.edu>
2022-02-21 12:53:23 -0500

    bumped version to 0.7.2

--- regolith/__init__.py
+++ regolith/__init__.py
@@ -3,6 +3,6 @@ import xonsh.imphooks
 
 xonsh.imphooks.install_import_hooks()
 
-__version__ = '0.7.1'
+__version__ = '0.7.2'
 
 del xonsh
--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def main():
         description="A research group content management system",
         long_description=readme,
         license="CC0",
-        version='0.7.1',
+        version='0.7.2',
         author="Anthony Scopatz",
         maintainer="Anthony Scopatz",
         author_email="scopatz@gmail.com",
