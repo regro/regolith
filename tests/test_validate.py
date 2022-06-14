7c578f1122bf5539a06cac93d9fb63e19b262ec4
Nick Asker <n.g.asker@gmail.com>
2022-06-14 16:41:30 -0400

    Addresses issue by importing into test_validate.py CalledProcessError from the standard library

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -3,6 +3,9 @@ import sys
 from io import StringIO
 
 import pytest
+
+from subprocess import CalledProcessError
+
 from xonsh.lib import subprocess
 
 from regolith.main import main
@@ -60,6 +63,6 @@ def test_validate_bad(make_bad_db):
     os.chdir(repo)
     try:
         subprocess.check_output(["regolith", "validate"])
-    except subprocess.CalledProcessError as e:
+    except CalledProcessError as e:
         assert "Errors found in " in e.output
         assert "NO ERRORS IN DBS" not in e.output
