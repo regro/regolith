fd0b232493bd840a9f9a31c740d37d2675445c2c
Simon Billinge <simon.billinge@gmail.com>
2022-07-08 16:13:15 +0200

    change default user to sbillinge

--- tests/conftest.py
+++ tests/conftest.py
@@ -43,7 +43,7 @@ def make_db():
     with open("regolithrc.json", "w") as f:
         json.dump(
             {
-                "default_user_id": "scopatz",
+                "default_user_id": "sbillinge",
                 "groupname": "ERGS",
                 "databases": [
                     {
