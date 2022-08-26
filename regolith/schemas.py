00b4b52ccd33aa5f7d64fd6421f39ca256071142
Simon Billinge <simon.billinge@gmail.com>
2022-08-26 19:27:03 +0200

    progress is not a list in milestones but slides_url is

--- regolith/schemas.py
+++ regolith/schemas.py
@@ -4074,23 +4074,20 @@ SCHEMAS = {
                               "type": "list"},
                     "progress": {"description": "update on the milestone",
                                  "required": False,
-                                 "type": "list",
+                                 "type": "dict",
                                  "schema": {
-                                     "type": "dict",
-                                     "schema": {
-                                         "text": {"description": "text description of progress and observations",
-                                                  "required": False,
-                                                  "type": "string"},
-                                         "figure": {"description": "token that dereferences a figure or image "
-                                                                   "in group local storage db",
+                                     "text": {"description": "text description of progress and observations",
+                                              "required": False,
+                                              "type": "string"},
+                                     "figure": {"description": "token that dereferences a figure or image "
+                                                               "in group local storage db",
+                                                "required": False,
+                                                "type": "string"},
+                                     "slides_url": {"description": "urls to slides describing the development, "
+                                                                   "e.g., Google slides url",
                                                     "required": False,
-                                                    "type": "string"},
-                                         "slides_url": {"description": "url to slides describing the development, "
-                                                                       "e.g., Google slides url",
-                                                        "required": False,
-                                                        "type": "string"}
+                                                    "type": "list"}
                                      }
-                                 }
                                  },
                     "objective": {"description": "explains goal of the milestone",
                                   "required": False,
