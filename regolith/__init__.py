053610f43e5ddd3c05f89fa210919557755d2b81
Simon Billinge <simon.billinge@gmail.com>
2022-09-16 10:02:24 -0400

    xonsh.imphooks.install_import_hooks(execer=None)

--- regolith/__init__.py
+++ regolith/__init__.py
@@ -2,7 +2,7 @@
 import xonsh.imphooks
 from xonsh.built_ins import XSH
 
-xonsh.imphooks.install_import_hooks(execer=XSH.execer)
+xonsh.imphooks.install_import_hooks(execer=None)
 
 __version__ = '0.5.1'
 
