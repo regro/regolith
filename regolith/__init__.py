c37beb7789c0c9905c6853b28830fb97c9200f10
Simon Billinge <simon.billinge@gmail.com>
2022-09-16 11:26:50 -0400

    cleaner implementation

--- regolith/__init__.py
+++ regolith/__init__.py
@@ -3,11 +3,8 @@ import xonsh.imphooks
 from xonsh.built_ins import XSH
 from xonsh.execer import Execer
 
+XSH.load(execer=Execer())
 execer = XSH.execer
-if execer is None:
-    execer = Execer()
-    XSH.load(execer=execer)
-
 xonsh.imphooks.install_import_hooks(execer=execer)
 
 __version__ = '0.5.1'
