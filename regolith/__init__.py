b4480ca6195ba40f22cd0ee83355842851806c61
Simon Billinge <simon.billinge@gmail.com>
2022-09-16 11:14:16 -0400

    now seems to be running

--- regolith/__init__.py
+++ regolith/__init__.py
@@ -1,7 +1,14 @@
 # stup import hooks
 import xonsh.imphooks
+from xonsh.built_ins import XSH
+from xonsh.execer import Execer
 
-xonsh.imphooks.install_import_hooks(execer=None)
+execer = XSH.execer
+if execer is None:
+    execer = Execer()
+    XSH.load(execer=execer)
+
+xonsh.imphooks.install_import_hooks(execer=execer)
 
 __version__ = '0.5.1'
 
