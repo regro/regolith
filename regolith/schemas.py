033e8d58cdc0a2a2364317457d2dfdfef2d09d2f
Simon Billinge <simon.billinge@gmail.com>
2023-10-27 12:56:13 -0400

    updating all the bibs with the new entry so old tests pass

--- regolith/schemas.py
+++ regolith/schemas.py
@@ -160,7 +160,7 @@ EXEMPLARS = {
             "doi": "tbd",
             "entrytype": "article",
             "journal": "PeerJ Computer Science",
-            "facilities": "nslsii, xpd",
+            "facility": "nslsii, xpd",
             "month": "tbd",
             "pages": "e103",
             "publisher": "PeerJ Inc. San Francisco, USA",
