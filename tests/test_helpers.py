68a6347a76d43a5b57b0ff891255d39cd5a46ea1
davidkim1 <davidekimb15@gmail.com>
2023-11-14 08:36:01 -0500

    added tests for f_todo with uuid passed as index

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -575,8 +575,11 @@ helper_map = [
     (["helper", "f_todo", "--index", "99100"],
      "WARNING: indices >= 9900 are used for milestones which should be finished using u_milestone and not f_todo\n"
      ),
-    (["helper", "f_todo", "1saefa"],
+    (["helper", "f_todo",  "--index", "1saefa"],
      "The task \"(1saefa) test a_todo\" in test for sbillinge has been marked as finished.\n"
+     ),
+    (["helper", "f_todo", "--index", "1saef"],
+     "Please enter either an index of todo or the first 6 characters of a todo\'s uuid..\n"
      ),
     (["helper", "u_todo", "--index", "3", "--assigned-to", "sbillinge",
       "--description", "update the description", "--due-date", "2020-07-06",
