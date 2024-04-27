d6487ee16580d86c561e0ad085649c15777fec14
Simon Billinge <simon.billinge@gmail.com>
2024-04-27 12:02:04 -0400

    adding __version__ back for now

--- regolith/__init__.py
+++ regolith/__init__.py
@@ -1,10 +1,2 @@
-# stup import hooks
-import xonsh.imphooks
-from xonsh.built_ins import XSH
-from xonsh.execer import Execer
 
-XSH.load(execer=Execer())
-execer = XSH.execer
-xonsh.imphooks.install_import_hooks(execer=execer)
-
-del xonsh
+__version__ = '0.8.2'
