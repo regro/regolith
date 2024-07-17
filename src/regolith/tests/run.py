56b7f027e31bd6587a64abcd5932389377671889
stevenhua0320 <r.hua@mail.utoronto.ca>
2024-07-18 02:39:15 +0800

    doc and test dir files

---
+++ src/regolith/tests/run.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) 2024 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Billinge Group members and community contributors.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/regro/regolith/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
+"""Convenience module for executing all unit tests with
+python -m regolith.tests.run
+"""
+
+import sys
+
+import pytest
+
+if __name__ == "__main__":
+    # show output results from every test function
+    args = ["-v"]
+    # show the message output for skipped and expected failure tests
+    if len(sys.argv) > 1:
+        args.extend(sys.argv[1:])
+    print("pytest arguments: {}".format(args))
+    # call pytest and exit with the return code from pytest
+    exit_res = pytest.main(args)
+    sys.exit(exit_res)
+
+# End of file
