b74a27eb5b841a032a2ded2fbdcae8ea0875c156
stevenhua0320 <r.hua@mail.utoronto.ca>
2024-07-18 01:23:53 +0800

    cookiecut at the step mv .git directory and copy docs/ dir file to doc dir

---
+++ src/regolith/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) 2024 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Billinge Group members and community contributors.
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/regro/regolith/graphs/contributors
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
+
+"""A Group System Management System"""
+
+# package version
+from regolith.version import __version__
+
+# silence the pyflakes syntax checker
+assert __version__ or True
+
+# End of file
