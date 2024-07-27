3258d058f3edaf3508b8839c86bdb2d1a3f47dde
stevenhua0320 <r.hua@mail.utoronto.ca>
2024-07-27 13:56:43 +0800

    update to remove xonsh deprecation warning

--- src/regolith/helpers/basehelper.py
+++ src/regolith/helpers/basehelper.py
@@ -5,7 +5,7 @@ from glob import glob
 from itertools import groupby
 
 from jinja2 import Environment, FileSystemLoader
-from xonsh.lib import subprocess
+from xonsh.api import subprocess
 
 from regolith.sorters import category_val, date_key, doc_date_key, level_val
 from regolith.tools import LATEX_OPTS, date_to_rfc822, gets, latex_safe, latex_safe_url, month_and_year, rfc822now
