4c1f6b8227eab94c63dc5a1669df4cdba52c42b4
tinatn29 <77407300+tinatn29@users.noreply.github.com>
2024-10-28 23:07:45 -0400

    Cookierelease 2 (#1168)
    * use cookiecutter flake8
    
    * edit version.py
    
    * use cookiecutter text.txt
    
    * delete run.txt (already have conda.txt)
    
    * use cookiecutter README
    
    * combine MANIFEST from old version and cookiecutter
    
    * remove recursive-include docs from MANIFEST.in
    
    * add mock, pytest-mock, requests-mock back in
    
    * add per-file-ignores back in
    
    * added the missing conda install line back in

--- src/regolith/version.py
+++ src/regolith/version.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 ##############################################################################
 #
-# regolith         by DANSE Diffraction group
-#                   Simon J. L. Billinge
-#                   (c) 2011-2024 The Trustees of Columbia University
-#                   in the City of New York.  All rights reserved.
+# (c) 2024 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
 #
-# File coded by:    Pavol Juhas
+# File coded by: Pavol Juhas, Billinge Group members and community contributors.
 #
-# See AUTHORS.txt for a list of people who contributed.
-# See LICENSE_DANSE.txt for license information.
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/regro/regolith/graphs/contributors
+#
+# See LICENSE.rst for license information.
 #
 ##############################################################################
 
-"""
-Definition of __version__
-"""
+"""Definition of __version__."""
+
+#  We do not use the other three variables, but can be added back if needed.
+#  __all__ = ["__date__", "__git_commit__", "__timestamp__", "__version__"]
 
 # obtain version information
 from importlib.metadata import version
