8581fb40c721a6025deb317b559b47c9a164cf30
Zhi Ming Xu <zhimingx27@gmail.com>
2025-06-21 20:06:11 -0400

    skpkg: migrate src folder

--- src/regolith/__init__.py
+++ src/regolith/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 ##############################################################################
 #
-# (c) 2024 The Trustees of Columbia University in the City of New York.
+# (c) 2025 The Trustees of Columbia University in the City of New York.
 # All rights reserved.
 #
-# File coded by: Billinge Group members and community contributors.
+# File coded by: Billinge Group and community contributors.
 #
 # See GitHub contributions for a more detailed list of contributors.
 # https://github.com/regro/regolith/graphs/contributors
@@ -12,23 +12,12 @@
 # See LICENSE.rst for license information.
 #
 ##############################################################################
-
-from xonsh.main import setup
+"""Python package for research group content management system."""
 
 # package version
-from regolith.version import __version__
-
-"""A Research group database management system"""
+from regolith.version import __version__  # noqa
 
 # silence the pyflakes syntax checker
 assert __version__ or True
 
-setup()
-del setup
-
-# Initialize the Xonsh environment
-# # execer = Execer(config=None)
-# # XSH.load(execer=execer)
-# # xonsh.imphooks.install_import_hooks(execer=execer)
-#
-# del xonsh
+# End of file
--- src/regolith/version.py
+++ src/regolith/version.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 ##############################################################################
 #
-# (c) 2024 The Trustees of Columbia University in the City of New York.
+# (c) 2025 The Trustees of Columbia University in the City of New York.
 # All rights reserved.
 #
-# File coded by: Pavol Juhas, Billinge Group members and community contributors.
+# File coded by: Billinge Group and community contributors.
 #
 # See GitHub contributions for a more detailed list of contributors.
-# https://github.com/regro/regolith/graphs/contributors
+# https://github.com/regro/regolith/graphs/contributors  # noqa: E501
 #
 # See LICENSE.rst for license information.
 #
